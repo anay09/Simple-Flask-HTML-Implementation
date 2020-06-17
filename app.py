87b7db1b8d2b6c12dd8f1a7c1d097774e3ca50b7
anayc <anay.chopade>
2020-06-17 10:28:30 +0100

    Created index.html file and added a placeholder value to the file. The origin of this placeholder is the inputting of a name. The output on the web server is Hello {{name}}. Perfectly functioning. Introduced the render_template library.

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    name = input('What is your name : ')
+    return render_template("index.html", name=name)
